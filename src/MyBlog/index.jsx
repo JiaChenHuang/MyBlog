b3b7cc8c85eca27797b8a7804c5b60c1c4f64ee6
JiaChenHuang <jiachen.huang@foxmail.com>
2020-11-19 13:38:31 +0800

    init个人博客

---
+++ src/MyBlog/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div>
+        <h2>Hello</h2>
+        <p>大家好，这是黄家臣的个人博客，现在正处于开发中</p>
+        <p>请大家耐心等待。。。</p>
+        <p>^ . ^</p>
+    </div>
+  )
+}
